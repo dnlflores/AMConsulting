3c062f420240d4f66ac5c93470727d919c4b1cef
Daniel Flores <daniel.flores4930@gmail.com>
2023-03-25 14:52:44 -0400

    implemented adapters and got auth working again:

---
+++ app/(auth)/signin/page.js
@@ -0,0 +1,10 @@
+import GoogleLoginButton from "@/app/components/GoogleLoginButton";
+
+export default function SignInForm() {
+
+    return (
+        <form>
+            <GoogleLoginButton />
+        </form>
+    )
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,27 +1,36 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from '@/lib/prisma'
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
-    })
-    // ...add more providers here
-  ],
-};
+export default async function auth(req, res) {
+    return await NextAuth(req, res, {
+        providers: [
+            GithubProvider({
+                clientId: process.env.GITHUB_ID,
+                clientSecret: process.env.GITHUB_SECRET,
+            }),
+            GoogleProvider({
+                clientId: process.env.GOOGLE_ID,
+                clientSecret: process.env.GOOGLE_SECRET,
+                authorization: {
+                    params: {
+                        prompt: "consent",
+                        access_type: "offline",
+                        response_type: "code"
+                    }
+                }
+            })
+            // ...add more providers here
+        ],
 
-export default NextAuth(authOptions);
+        pages: {
+            signIn: '/signin'
+        },
+        adapter: PrismaAdapter(prisma),
+        session: {
+            strategy: 'jwt'
+        }
+    })
+}
